ebe92866a6251a392db8de3f2e88c7c59edd6e00
dabin <db021129@naver.com>
2024-01-07 17:21:39 +0900

    first commit

---
+++ hoho/src/main/java/hoho_project/hoho/HohoApplication.java
@@ -0,0 +1,13 @@
+package hoho_project.hoho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HohoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HohoApplication.class, args);
+	}
+
+}
