cb98774480c395df280aad0043005df3162e1935
dabin <db021129@naver.com>
2024-01-08 16:47:17 +0900

    생성 메서드 작성

--- hoho/src/main/java/hoho_project/hoho/domain/Menu.java
+++ hoho/src/main/java/hoho_project/hoho/domain/Menu.java
@@ -27,4 +27,19 @@ public class Menu {
     private String menu_ingredient;
 
     private Long menu_price;
+
+    //==생성 메서드==//
+    public static Menu createMenu(String menu_type, String menu_imgName,byte[] menu_image,
+                                  String menu_name, String menu_ingredient, Long menu_price) {
+        Menu menu = new Menu();
+
+        menu.setMenu_type(menu_type);
+        menu.setMenu_imgName(menu_imgName);
+        menu.setMenu_image(menu_image);
+        menu.setMenu_name(menu_name);
+        menu.setMenu_ingredient(menu_ingredient);
+        menu.setMenu_price(menu_price);
+
+        return menu;
+    }
 }
