3cc66defbc56f0ef03091de9d2d5280e6538bcaa
dabin <db021129@naver.com>
2024-01-08 16:25:28 +0900

    HomeService 작성

---
+++ hoho/src/main/java/hoho_project/hoho/service/HomeService.java
@@ -0,0 +1,37 @@
+package hoho_project.hoho.service;
+
+import hoho_project.hoho.domain.Home;
+import hoho_project.hoho.repository.HomeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class HomeService {
+
+    private final HomeRepository homeRepository;
+
+    // 사진 등록
+    @Transactional
+    public Home saveHome(String home_imgName, byte[] home_image) {
+
+        // home 생성
+        Home home = Home.createHome(home_imgName, home_image);
+
+        // home 저장
+        homeRepository.save(home);
+
+        return home;
+    }
+
+    // 사진 삭제
+    @Transactional
+    public Home deleteHome(Home home) {
+        homeRepository.delete(home);
+
+        return home;
+    }
+
+}
